381fdec5468b5963180737b86221cdf74a2455a4
Darryl Wagoner <darryl@wagoner.me>
2023-08-13 18:41:14 -0500

    cp2

--- LogGate/ViewModel/MainViewModel.cs
+++ LogGate/ViewModel/MainViewModel.cs
@@ -1,20 +1,27 @@
 ﻿
-
-
-using CommunityToolkit.Mvvm.ComponentModel;
-
 namespace LogGate.ViewModel
 {
     public partial class MainViewModel : ObservableObject
     {
+        ObservableCollection<string> items;
+
         [ObservableProperty]
         Qso qso;
         [ObservableProperty]
         string text;
 
+
+        public MainViewModel()
+        {
+            items = new ObservableCollection<string>();
+        }
+        [RelayCommand]
         void Add()
         {
-            text = string.Empty;
+            if (Text.IsNullOrEmpty())
+                return;
+            items.Add(Text);
+            Text = string.Empty;
         }
     }
 }
--- LogGate/zGlobalUsing.cs
+++ LogGate/zGlobalUsing.cs
@@ -1,2 +1,5 @@
 ﻿
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
 global using HamDevLib;
+global using System.Collections.ObjectModel;
