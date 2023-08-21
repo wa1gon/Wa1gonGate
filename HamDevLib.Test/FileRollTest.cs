68f158e10cb29a57594bf82c6494d96e9ab87e3d
Darryl Wagoner <darryl@wagoner.me>
2023-08-21 10:13:17 -0500

    Added file rolling method

---
+++ HamDevLib.Test/FileRollTest.cs
@@ -0,0 +1,42 @@
+﻿using HamDotNetToolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamDevLib.Test;
+[TestClass]
+public class FileRollTest
+{
+    private const string TestDirectory = @"C:\Temp\TestRolling";
+    private const string BaseFileName = "testfile.txt";
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext context)
+    {
+        Directory.CreateDirectory(TestDirectory);
+        File.Create(Path.Combine(TestDirectory, BaseFileName)).Close();
+    }
+    [TestMethod]
+    public void TestRollingAndCleanup()
+    {
+        int maxRolls = 2; // Maximum number of rolled files to keep
+        FileToolkit.RollandCleanupFiles(Path.Combine(TestDirectory, BaseFileName), maxRolls);
+
+        // Check if the rolled file was created
+        Assert.IsTrue(File.Exists(Path.Combine(TestDirectory, "testfile_1.txt")));
+
+        // Check if the old file was deleted
+        Assert.IsFalse(File.Exists(Path.Combine(TestDirectory, "testfile.txt")));
+
+        // Check if the cleanup was performed as expected
+        Assert.IsFalse(File.Exists(Path.Combine(TestDirectory, "testfile_0.txt")));
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        Directory.Delete(TestDirectory, true);
+    }
+}
---
+++ HamDotNetToolkit/FileRoll.cs
@@ -0,0 +1,45 @@
+﻿namespace HamDotNetToolkit
+{
+    public class FileToolkit
+    {
+
+        public static void RollandCleanupFiles(string baseFileName, int maxRolls)
+        {
+            if (baseFileName.IsNullOrEmpty())
+                return;
+
+            string directoryPath = Path.GetDirectoryName(baseFileName);
+            if ( directoryPath.IsNullOrEmpty())
+            {
+                return;
+            }
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(baseFileName);
+            string extension = Path.GetExtension(baseFileName);
+
+            var existingFiles = Directory.GetFiles(directoryPath, $"{fileNameWithoutExtension}_*{extension}")
+                                         .Select(Path.GetFileName)
+                                         .ToList();
+
+            // Determine the next available roll count
+            int nextRollCount = 1;
+            while (existingFiles.Contains($"{fileNameWithoutExtension}_{nextRollCount}{extension}"))
+            {
+                nextRollCount++;
+            }
+
+            // Roll the file
+            string rolledFileName = $"{fileNameWithoutExtension}_{nextRollCount}{extension}";
+            string fullRolledFilePath = Path.Combine(directoryPath, rolledFileName);
+            File.Move(baseFileName, fullRolledFilePath);
+
+            // Cleanup old rolled files
+            var filesToDelete = existingFiles.Take(Math.Max(0, existingFiles.Count - maxRolls));
+            foreach (var fileToDelete in filesToDelete)
+            {
+                if (filesToDelete.IsNullOrEmpty())
+                    continue;
+                File.Delete(Path.Combine(directoryPath, fileToDelete));
+            }
+        }
+    }
+}
