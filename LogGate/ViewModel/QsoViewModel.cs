0517cec52720f9e192b8d3e7cccd97aed2abde9c
Darryl Wagoner <darryl@wagoner.me>
2023-08-27 12:29:42 -0500

    added modes

--- LogGate/ViewModel/QsoViewModel.cs
+++ LogGate/ViewModel/QsoViewModel.cs
@@ -26,6 +26,51 @@ namespace LogGate.ViewModel
         [ObservableProperty]
         public ICollection<QsoDetail> qsoDetails = new List<QsoDetail>();
 
+        public List<string> ModeList { get; set; } = new List<string>()
+        {
+                "AM",
+                "ARDOP",
+                "APRS",
+                "ATV",
+                "CLO",
+                "CONTESTI",
+                "CW",
+                "DIGITALVOICE",
+                "DOMINO",
+                "DYNAMIC",
+                "FAX",
+                "FM",
+                "FSK441",
+                "FT4",
+                "FT8",
+                "HELL",
+                "ISCAT",
+                "JT6M",
+                "JT44",
+                "JT65",
+                "JT9",
+                "JS8Call",
+                "MFSK",
+                "MT63",
+                "OLIVIA",
+                "PAC",
+                "PAX",
+                "PKT",
+                "PSK31",
+                "PSK2K",
+                // need more
+                "Radar",
+                "ROS",
+                "RTTY",
+                "SSTV",
+                "TOR",
+                "Throb",
+                "Thor",
+                "WSPR",
+                "WSJT-X",
+ 
+                // Add more modes here
+            };
         private void ForceUppercase(object sender, TextChangedEventArgs e)
         {
             if (sender is Entry entry)
